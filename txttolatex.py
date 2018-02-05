93703f099432c256525626a072440553c687a0e7
gettecr <36174590+gettecr@users.noreply.github.com>
2018-02-05 16:44:07 -0600

    Update txttolatex.py

--- txttolatex.py
+++ txttolatex.py
@@ -9,15 +9,18 @@ datafile = "./test.txt"
 outputfile = "./latexout.txt"
 
 #Change tableFormat to set an alternative format (e.g. 'longtable') 
-#Default is 'tabular'; currently only 'longtable' is available
+#Default is 'tabular'; currently 'tabular' or 'longtable' are available
 tableFormat = 'tabular'
 
-def convert(datafile, outputfile, colnums= [-1], align = 'l', style = 'tabular'):
+#set alignment of columns: 'l', 'c', 'r'
+align = 'l'
+
+def convert(datafile, outputfile, colnums= [-1], align = align, style = tableFormat):
     if style == 'tabular':
         tabular.out(datafile=datafile, outputfile=outputfile, colnums=colnums, align= align)
     elif style == 'longtable':
         longtable.out(datafile=datafile, outputfile=outputfile, colnums=colnums, align=align)
 
 
-convert(datafile, outputfile, style = tableFormat)
+convert(datafile, outputfile)
 
